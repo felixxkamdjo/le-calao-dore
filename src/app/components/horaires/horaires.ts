97edf08f2abb2725b835a3ddc410a1b1ad0145b7
felixxkamdjo <felixxkamdjo@gmail.com>
2026-07-27 20:11:44 +0100

    feat(horaires): section horaires avec statut jour actuel

---
+++ src/app/components/horaires/horaires.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+interface Horaire {
+  jour: string;
+  ouverture: string;
+  fermeture: string;
+  ferme: boolean;
+}
+
+@Component({
+  selector: 'app-horaires',
+  imports: [],
+  templateUrl: './horaires.html',
+  styleUrl: './horaires.css',
+})
+export class Horaires {
+  public horaires: Horaire[] = [
+    { jour: 'Lundi', ouverture: '11h00', fermeture: '22h00', ferme: false },
+    { jour: 'Mardi', ouverture: '11h00', fermeture: '22h00', ferme: false },
+    { jour: 'Mercredi', ouverture: '11h00', fermeture: '22h00', ferme: false },
+    { jour: 'Jeudi', ouverture: '11h00', fermeture: '22h00', ferme: false },
+    { jour: 'Vendredi', ouverture: '11h00', fermeture: '23h00', ferme: false },
+    { jour: 'Samedi', ouverture: '11h00', fermeture: '23h00', ferme: false },
+    { jour: 'Dimanche', ouverture: '', fermeture: '', ferme: true }
+  ];
+
+  public joursFR = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+
+  get jourActuel(): string {
+    return this.joursFR[new Date().getDay()];
+  }
+}
